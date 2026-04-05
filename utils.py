00380bd3638f96918ae4052677691db61f4f1a1e
Tilek Kubanychbekov <tkubanyc@2-D-3.42heilbronn.de>
2026-04-05 11:28:24 +0200

    Refactor utils.py

--- utils.py
+++ utils.py
@@ -1,73 +1,89 @@
-# Load data from file
 def load_data(filepath):
-    X = []
-    Y = []
+    """
+    Load dataset from CSV file.
+    Returns:
+        X (list of float): mileage
+        Y (list of float): price
+    """
+    X, Y = [], []
 
     with open(filepath, "r") as file:
-        lines = file.readlines()
+        next(file)  # skip header
 
-        #skip header
-        for line in lines[1:]:
-            # remove \n
+        for line in file:
             line = line.strip()
-
             if not line:
                 continue
 
-            # separate values
             mileage, price = line.split(",")
-
             X.append(float(mileage))
             Y.append(float(price))
 
     return X, Y
 
-# theta0 - base price (starting value)
-# theta1 - how price changes with mileage
-def estimate_price(mileage, theta0, theta1):
-    return theta0 + theta1 * mileage
 
-# Normalize values
+def estimate_price(x, theta0, theta1):
+    """
+    Linear model: y = theta0 + theta1 * x
+    """
+    return theta0 + theta1 * x
+
+
 def normalize(X):
+    """
+    Normalize values using standardization:
+    x_norm = (x - mean) / std
+    """
     mean = sum(X) / len(X)
-    
     variance = sum((x - mean) ** 2 for x in X) / len(X)
     std = variance ** 0.5
 
-    X_norm = [(x - mean) / std for x in X]
+    if std == 0:
+        std = 1  # avoid division by zero
 
+    X_norm = [(x - mean) / std for x in X]
     return X_norm, mean, std
 
-# Calculate Mean Squared Error (MSE)
+
+# ---------- Evaluation Metrics ----------
+
 def compute_mse(X, Y, theta0, theta1):
+    """
+    Mean Squared Error:
+    average squared difference between prediction and real value
+    """
     m = len(X)
     error = 0.0
 
     for i in range(m):
-        pred = theta0 + theta1 * X[i]
+        pred = estimate_price(X[i], theta0, theta1)
         error += (pred - Y[i]) ** 2
 
     return error / m
 
-# Calculate Root Mean Squared Error (RMSE)
+
 def compute_rmse(X, Y, theta0, theta1):
-    mse = compute_mse(X, Y, theta0, theta1)
-    return mse ** 0,5
+    """
+    Root Mean Squared Error:
+    same as MSE but in original unit (price)
+    """
+    return compute_mse(X, Y, theta0, theta1) ** 0.5  # FIXED BUG
 
-# Calculate R² Score
+
 def compute_r2(X, Y, theta0, theta1):
+    """
+    R² Score:
+    measures how well the model explains variance in data
+    """
     m = len(X)
-
-    # mean of Y
-    mean_Y = sum(Y) / m
+    mean_y = sum(Y) / m
 
     ss_total = 0.0
     ss_residual = 0.0
 
     for i in range(m):
-        pred = theta0 + theta1 * X[i]
-
+        pred = estimate_price(X[i], theta0, theta1)
         ss_residual += (Y[i] - pred) ** 2
-        ss_total += (Y[i] - mean_Y) ** 2
+        ss_total += (Y[i] - mean_y) ** 2
 
     return 1 - (ss_residual / ss_total)
